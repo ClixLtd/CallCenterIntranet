ad016a6638f83431eee3362889cf944b544e8124
Simon Skinner <simonskinner@dk101.net>
2013-03-08 17:05:22 +0000

    Started work on the senior report

--- fuel/app/classes/controller/reports.php
+++ fuel/app/classes/controller/reports.php
@@ -226,21 +226,30 @@ class Controller_Reports extends Controller_BaseHybrid
 	public function action_senior_report($center=null)
 	{
     	
-    	if (Auth::has_access('reports.all_centers')) {
-        	$view_all = TRUE;
-    	} else {
-        	$view_all = FALSE;
-    	}
+    	if (Auth::has_access('reports.disposition'))
     	
-    	$all_call_centers = Model_Call_Center::find('all');
-    	
-    	$this->template->title = 'Reports &raquo; Seniors';
-		$this->template->content = View::forge('reports/senior', array(
-		    'view_all' => $view_all,
-		    'all_call_centers' => $all_call_centers,
-		    'center' => $center,
-			'url' => (!is_null($center)) ? '/reports/get_senior_report/'.$center.'.json' : '/reports/get_senior_report.json',
-		));	
+        	if (Auth::has_access('reports.all_centers')) {
+            	$view_all = TRUE;
+        	} else {
+            	$view_all = FALSE;
+        	}
+        	
+        	$all_call_centers = Model_Call_Center::find('all');
+        	
+        	$this->template->title = 'Reports &raquo; Seniors';
+    		$this->template->content = View::forge('reports/senior', array(
+    		    'view_all' => $view_all,
+    		    'all_call_centers' => $all_call_centers,
+    		    'center' => $center,
+    			'url' => (!is_null($center)) ? '/reports/get_senior_report/'.$center.'.json' : '/reports/get_senior_report.json',
+    		));	
+		
+		}
+		else
+		{
+			Session::set_flash('fail', 'You do not have access to that section: This has been logged!');
+			Response::redirect('/');
+		}
     	
 	}
 	
@@ -585,22 +594,32 @@ class Controller_Reports extends Controller_BaseHybrid
 	
 	public function action_telesales_report($center=null)
 	{
+	
+	    if (Auth::has_access('reports.disposition'))
+	    {
     	
-    	if (Auth::has_access('reports.all_centers')) {
-        	$view_all = TRUE;
-    	} else {
-        	$view_all = FALSE;
-    	}
-    	
-    	$all_call_centers = Model_Call_Center::find('all');
-    	
-    	$this->template->title = 'Reports &raquo; Telesales';
-		$this->template->content = View::forge('reports/telesales', array(
-		    'view_all' => $view_all,
-		    'all_call_centers' => $all_call_centers,
-		    'center' => $center,
-			'url' => (!is_null($center)) ? '/reports/get_telesales_report/'.$center.'.json' : '/reports/get_telesales_report.json',
-		));	
+        	if (Auth::has_access('reports.all_centers')) {
+            	$view_all = TRUE;
+        	} else {
+            	$view_all = FALSE;
+        	}
+        	
+        	$all_call_centers = Model_Call_Center::find('all');
+        	
+        	$this->template->title = 'Reports &raquo; Telesales';
+    		$this->template->content = View::forge('reports/telesales', array(
+    		    'view_all' => $view_all,
+    		    'all_call_centers' => $all_call_centers,
+    		    'center' => $center,
+    			'url' => (!is_null($center)) ? '/reports/get_telesales_report/'.$center.'.json' : '/reports/get_telesales_report.json',
+    		));	
+		
+		}
+		else
+		{
+			Session::set_flash('fail', 'You do not have access to that section: This has been logged!');
+			Response::redirect('/');
+		}
     	
 	}
 	
