fd7692d9e071a8bcbbcd688c1211805821c2074f
Simon Skinner <simonskinner@dk101.net>
2013-03-19 14:23:33 +0000

    Test

--- fuel/app/tasks/adam.php
+++ fuel/app/tasks/adam.php
@@ -48,6 +48,9 @@
 		public function run()
 		{
 			$todayPayments = \DB::query("INSERT INTO `asterisk`.`vicidial_conferences` (`conf_exten`, `server_ip`, `extension`, `leave_3way`, `leave_3way_datetime`) VALUES ('8600706', '10.1.0.132', NULL, '0', NULL)")->execute('gabdialler');
+			
+			print_r($todayPayments);
+			
 		}
 		
 		
