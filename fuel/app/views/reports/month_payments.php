0da048c72c4a4e66c71e728db97bdc3b542dffcb
Simon Skinner <simonskinner@dk101.net>
2013-03-20 23:56:20 +0000

    Working on Payments Reports

---
+++ fuel/app/views/reports/month_payments.php
@@ -0,0 +1,58 @@
+<article class="full-block clearfix">
+
+	<div class="article-container">
+		<header>
+			<h2>Monthly Payments</h2>
+			
+			<nav>
+				<ul class="tab-switch">
+					<li><a class="default-tab" href="#quickview">Payments</a></li>
+				</ul>
+			</nav>
+			
+		</header>
+	</div>
+	
+	<section>
+		<div class="tab default-tab" id="quickview">
+			<article class="full-block">
+				<h3></h3>
+				
+				<article class="half-block">
+				
+				<div class="article-container">
+					<section>
+					<table class="zebra-striped">
+						<thead>
+							<tr>
+								<th>ClientID</th>
+								<th>Name</th>
+								<th>Amount In</th>
+								<th>Notes</th>
+							</tr>
+						</thead>
+						<tbody>
+						    <?php foreach($payments AS $pay): ?>
+							<tr>
+								<td><?php echo $pay['ClientID']; ?></td>
+								<td><?php echo $pay['Name']; ?></td>
+								<td><?php echo $pay['AmountIn']; ?></td>
+								<td><?php echo $pay['NormalExpectedPayment']; ?></td>
+							</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+					</section>
+				</div>
+				
+			</article>
+
+				
+			</article>
+		</div>
+
+	</section>
+	
+	
+
+</article>
