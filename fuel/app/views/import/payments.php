f2c0e4982e54f947083a0d2923f0594148b00186
Simon Skinner <simonskinner@dk101.net>
2013-04-03 13:31:00 +0100

    Change of month on payment report

---
+++ fuel/app/views/import/payments.php
@@ -0,0 +1,27 @@
+<article class="full-block clearfix">
+
+	<div class="article-container">
+		<header>
+			<h2>Importing Payments</h2>			
+		</header>
+	</div>
+	
+	<section>
+		<div class="tab default-tab" id="quickview">
+			<article class="full-block">
+
+    			<div>
+        			<span>0% complete</span>
+    			</div>
+    			
+    			To import a payment file please select the CSV file from sagepay on your computer using the Browse button below.
+    			
+    			<form action="/import/parse_payments/" method="post" enctype="multipart/form-data">
+        			<input type="file" name="file">
+        			<input type="submit" value="Upload">
+    			</form>
+    			
+			</article>
+		</div>
+	</section>
+</article>
