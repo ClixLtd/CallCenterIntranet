9038b8867fa1a4b11cbbaef78a69cc79fc0e70e4
Simon Skinner <simonskinner@dk101.net>
2013-04-22 19:27:02 +0100

    New values decided on and updated

--- fuel/app/classes/controller/dialler/api.php
+++ fuel/app/classes/controller/dialler/api.php
@@ -157,90 +157,35 @@
 		
 		public function get_mobile_wallboard()
 		{
-			$all_last_month = GAB\Debtsolv::get_referral_count(null, date("01-m-Y", strtotime("Last Month")), date("t-m-Y", strtotime("Last Month")), 604800);
-			$all_this_month = GAB\Debtsolv::get_referral_count(null, date("01-m-Y"), date("t-m-Y"), 86400);
-			$all_this_week = GAB\Debtsolv::get_referral_count(null, date("d-m-Y", strtotime("monday this week")), date("d-m-Y"), 900);
-			
-			$internal_today = GAB\Debtsolv::get_referral_count('INTERNAL');
 			
 			$hq_today = GAB\Debtsolv::get_referral_count('GAB');
 			$burton_today = GAB\Debtsolv::get_referral_count('RESOLVE');
 			$pcc_today = GAB\Debtsolv::get_referral_count('GBS');
 			
-			$rjfive_today = GAB\Debtsolv::get_referral_count('rj5');
-			$sixeleven_today = GAB\Debtsolv::get_referral_count('SixEleven');
-			$so_today = GAB\Debtsolv::get_referral_count('SO');
 			
 			$this->response(array(
-				'INTERNAL' => array(
-					'referrals' => $internal_today['referrals'],
-					'pack_out' => $internal_today['pack_outs'],
-					'di' => ($internal_today['pack_outs']==0) ? 0 : number_format($internal_today['pack_outs_value'] / $internal_today['pack_outs'],2),
-					'pack_out_percentage' => ($internal_today['referrals']==0) ? 0 : number_format((($internal_today['pack_outs']/$internal_today['referrals'])*100),2),
-					'pack_out_value' => number_format($internal_today['pack_outs_value'],2),
-				),
 				'HQ' => array(
 					'referrals' => $hq_today['referrals'],
 					'pack_out' => $hq_today['pack_outs'],
-					'di' => ($hq_today['pack_outs']==0) ? 0 : number_format($hq_today['pack_outs_value'] / $hq_today['pack_outs'],2),
 					'pack_out_percentage' => ($hq_today['referrals']==0) ? 0 : number_format((($hq_today['pack_outs']/$hq_today['referrals'])*100),2),
-					'pack_out_value' => number_format($hq_today['pack_outs_value'],2),
 				),
 				'RESOLVE' => array(
 					'referrals' => $burton_today['referrals'],
 					'pack_out' => $burton_today['pack_outs'],
-					'di' => ($burton_today['pack_outs']==0) ? 0 : number_format($burton_today['pack_outs_value'] / $burton_today['pack_outs'],2),
 					'pack_out_percentage' => ($burton_today['referrals']==0) ? 0 : number_format((($burton_today['pack_outs']/$burton_today['referrals'])*100),2),
-					'pack_out_value' => number_format($burton_today['pack_outs_value'],2),
 				),
 				'PCC' => array(
 					'referrals' => $pcc_today['referrals'],
 					'pack_out' => $pcc_today['pack_outs'],
-					'di' => ($pcc_today['pack_outs']==0) ? 0 : number_format($pcc_today['pack_outs_value'] / $pcc_today['pack_outs'],2),
-					'pack_out_percentage' => ($pcc_today['referrals']==0) ? 0 : number_format((($pcc_today['pack_outs']/$pcc_today['referrals'])*100),2),
-					'pack_out_value' => number_format($pcc_today['pack_outs_value'],2),
-				),
-				'RJ5' => array(
-					'referrals' => $rjfive_today['referrals'],
-					'pack_out' => $rjfive_today['pack_outs'],
-					'di' => ($rjfive_today['pack_outs']==0) ? 0 : number_format($rjfive_today['pack_outs_value'] / $rjfive_today['pack_outs'],2),
-					'pack_out_percentage' => ($rjfive_today['referrals']==0) ? 0 : number_format((($rjfive_today['pack_outs']/$rjfive_today['referrals'])*100),2),
-					'pack_out_value' => number_format($rjfive_today['pack_outs_value'],2),
-				),
-				'SO' => array(
-					'referrals' => $so_today['referrals'],
-					'pack_out' => $so_today['pack_outs'],
-					'di' => ($so_today['pack_outs']==0) ? 0 : number_format($so_today['pack_outs_value'] / $so_today['pack_outs'],2),
-					'pack_out_percentage' => ($so_today['referrals']==0) ? 0 : number_format((($so_today['pack_outs']/$so_today['referrals'])*100),2),
-					'pack_out_value' => number_format($so_today['pack_outs_value'],2),
-				),
-				'SIXELEVEN' => array(
-					'referrals' => $sixeleven_today['referrals'],
-					'pack_out' => $sixeleven_today['pack_outs'],
-					'di' => ($sixeleven_today['pack_outs']==0) ? 0 : number_format($sixeleven_today['pack_outs_value'] / $sixeleven_today['pack_outs'],2),
-					'pack_out_percentage' => ($sixeleven_today['referrals']==0) ? 0 : number_format((($sixeleven_today['pack_outs']/$sixeleven_today['referrals'])*100),2),
-					'pack_out_value' => number_format($sixeleven_today['pack_outs_value'],2),
+					'pack_out_percentage' => ($pcc_today['referrals']==0) ? 0 : number_format((($pcc_today['pack_outs']/$pcc_today['referrals'])*100),2),				),
+				'COMBINED' => array(
+					'referrals' => ($hq_today['referrals'] + $burton_today['referrals'] + $pcc_today['referrals']),
+					'pack_out' => ($hq_today['pack_outs'] + $burton_today['pack_outs'] + $pcc_today['pack_outs']),
+					'pack_out_percentage' => (($hq_today['referrals'] + $burton_today['referrals'] + $pcc_today['referrals']) < 1) ? 0 : number_format(($hq_today['pack_outs'] + $burton_today['pack_outs'] + $pcc_today['pack_outs']) / ($hq_today['referrals'] + $burton_today['referrals'] + $pcc_today['referrals']),2),
+					//($hq_today['referrals']==0) ? 0 : number_format((($hq_today['pack_outs']/$hq_today['referrals'])*100),2),
 				),
 				
 				
-				'last_month' => array(
-					'referrals' => $all_last_month['referrals'],
-					'pack_out' => $all_last_month['pack_outs'],
-					'pack_out_percentage' => ($all_last_month['referrals']==0) ? 0 : number_format((($all_last_month['pack_outs']/$all_last_month['referrals'])*100),2),
-					'pack_out_value' => number_format($all_last_month['pack_outs_value'],2),
-				),
-				'this_month' => array(
-					'referrals' => $all_this_month['referrals'],
-					'pack_out' => $all_this_month['pack_outs'],
-					'pack_out_percentage' => ($all_this_month['referrals']==0) ? 0 : number_format((($all_this_month['pack_outs']/$all_this_month['referrals'])*100),2),
-					'pack_out_value' => number_format($all_this_month['pack_outs_value'],2),
-				),
-				'this_week' => array(
-					'referrals' => $all_this_week['referrals'],
-					'pack_out' => $all_this_week['pack_outs'],
-					'pack_out_percentage' => ($all_this_week['referrals']==0) ? 0 : number_format((($all_this_week['pack_outs']/$all_this_week['referrals'])*100),2),
-					'pack_out_value' => number_format($all_this_week['pack_outs_value'],2),
-				),
 			));
 			
 		}
