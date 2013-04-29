8c0532194dd1e72863bcdde54a9b3d8cb05d8dad
Simon Skinner <simonskinner@dk101.net>
2013-04-29 18:04:38 +0100

    Removing PPI from telesales report

--- fuel/app/classes/controller/reports.php
+++ fuel/app/classes/controller/reports.php
@@ -905,7 +905,6 @@ GROUP BY
                           WHERE DR.user_login IN (" . $inList . ")
                               AND DR.short_code IN ('GAB','GBS', '1TICK', '1TICK-GBS')
                               AND TCR.[Description] <> 'Referred'
-                              AND D_CPD.NormalExpectedPayment > 0
                               AND CONVERT(date, DR.referral_date, 105) >= '" . $startDate . "'
                               AND CONVERT(date, DR.referral_date, 105) <= '" . $endDate . "'";
     	
@@ -942,7 +941,6 @@ GROUP BY
                   WHERE DR.user_login IN (" . $inList . ")
                       AND DR.short_code IN ('RESOLVE')
                       AND TCR.[Description] <> 'Referred'
-                      AND D_CPD.NormalExpectedPayment > 0
                       AND CONVERT(date, DR.referral_date, 105) >= '" . $startDate . "'
                       AND CONVERT(date, DR.referral_date, 105) <= '" . $endDate . "'";
     	
@@ -958,7 +956,6 @@ GROUP BY
                           LEFT JOIN Dialler.dbo.referrals AS D_R ON D_CLD.LeadPoolReference = D_R.leadpool_id
                           WHERE D_R.user_login IN (" . $inList . ")
                               AND D_R.short_code IN ('GAB','GBS', '1TICK', '1TICK-GBS')
-                              AND D_CPD.NormalExpectedPayment > 0
                               AND CONVERT(date, D_CD.FirstPaymentDate, 105) >= '" . $startDate . "'
                               AND CONVERT(date, D_CD.FirstPaymentDate, 105) <= '" . $endDate . "'";
     	
@@ -974,7 +971,6 @@ GROUP BY
                           LEFT JOIN Dialler.dbo.referrals AS D_R ON D_CLD.LeadPoolReference = D_R.leadpool_id
                           WHERE D_R.user_login IN (" . $inList . ")
                               AND D_R.short_code IN ('RESOLVE')
-                              AND D_CPD.NormalExpectedPayment > 0
                               AND CONVERT(date, D_CD.FirstPaymentDate, 105) >= '" . $startDate . "'
                               AND CONVERT(date, D_CD.FirstPaymentDate, 105) <= '" . $endDate . "'";
     	
