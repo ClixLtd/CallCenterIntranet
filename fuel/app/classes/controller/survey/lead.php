634fe4fdb59be5fb6bcf48ac1f3d1bac368d98c1
Simon Skinner <simonskinner@dk101.net>
2013-05-20 16:57:10 +0100

    Dupe Checking added

--- fuel/app/classes/controller/survey/lead.php
+++ fuel/app/classes/controller/survey/lead.php
@@ -83,142 +83,157 @@ class Controller_Survey_Lead extends Controller_Template
 	    
 	    foreach ($ppiLeads as $lead)
 	    {
-    	    $singleLead = \Model_Crmreferral::find($lead);
+	        $thisCheck = Model_Survey_Lead_Dialler::query()->where('referral_id', $lead);
     	    
-    	    $ppiLeadInsert = array(
-                'lead_id'                 => "",
-                'entry_date'              => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
-                'modify_date'             => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
-                'status'                  => "NEW",
-                'user'                    => "",
-                'vendor_lead_code'        => "",
-                'source_id'               => "",
-                'list_id'                 => 199999,
-                'gmt_offset_now'          => 0.00,
-                'called_since_last_reset' => "N",
-                'phone_code'              => "9",
-                'phone_number'            => (int)(is_null($singleLead->tel_home)) ? $singleLead->tel_mobile : $singleLead->tel_home,
-                'title'                   => "",
-                'first_name'              => $singleLead->forename,
-                'middle_initial'          => "",
-                'last_name'               => $singleLead->surname,
-                'address1'                => $singleLead->street_and_number,
-                'address2'                => $singleLead->area,
-                'address3'                => $singleLead->district,
-                'city'                    => $singleLead->town,
-                'state'                   => "",
-                'province'                => $singleLead->county,
-                'postal_code'             => $singleLead->post_code,
-                'country_code'            => "UK",
-                'gender'                  => "U",
-                'date_of_birth'           => date('Y-m-d', strtotime($singleLead->date_of_birth)),
-                'alt_phone'               => ((int)$singleLead->tel_mobile == 0) ? "" : (int)$singleLead->tel_mobile,
-                'email'                   => "",
-                'security_phrase'         => "",
-                'comments'                => "!!! SURVEY LEAD !!! - PPI QUALIFIED",
-                'called_count'            => 0,
-                'last_local_call_time'    => "2009-01-01 00:00:00",
-                'rank'                    => 0,
-                'owner'                   => "",
-                'entry_list_id'           => 0,
-            );
+    	    if ($thisCheck->count() < 1)
+    	    {
+        	    
+        	    $singleLead = \Model_Crmreferral::find($lead);
+        	    
+        	    $ppiLeadInsert = array(
+                    'lead_id'                 => "",
+                    'entry_date'              => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
+                    'modify_date'             => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
+                    'status'                  => "NEW",
+                    'user'                    => "",
+                    'vendor_lead_code'        => "",
+                    'source_id'               => "",
+                    'list_id'                 => 199999,
+                    'gmt_offset_now'          => 0.00,
+                    'called_since_last_reset' => "N",
+                    'phone_code'              => "9",
+                    'phone_number'            => (int)(is_null($singleLead->tel_home)) ? $singleLead->tel_mobile : $singleLead->tel_home,
+                    'title'                   => "",
+                    'first_name'              => $singleLead->forename,
+                    'middle_initial'          => "",
+                    'last_name'               => $singleLead->surname,
+                    'address1'                => $singleLead->street_and_number,
+                    'address2'                => $singleLead->area,
+                    'address3'                => $singleLead->district,
+                    'city'                    => $singleLead->town,
+                    'state'                   => "",
+                    'province'                => $singleLead->county,
+                    'postal_code'             => $singleLead->post_code,
+                    'country_code'            => "UK",
+                    'gender'                  => "U",
+                    'date_of_birth'           => date('Y-m-d', strtotime($singleLead->date_of_birth)),
+                    'alt_phone'               => ((int)$singleLead->tel_mobile == 0) ? "" : (int)$singleLead->tel_mobile,
+                    'email'                   => "",
+                    'security_phrase'         => "",
+                    'comments'                => "!!! SURVEY LEAD !!! - PPI QUALIFIED",
+                    'called_count'            => 0,
+                    'last_local_call_time'    => "2009-01-01 00:00:00",
+                    'rank'                    => 0,
+                    'owner'                   => "",
+                    'entry_list_id'           => 0,
+                );
+        	    
+        	    // Add leads directly to the dialler
+        	    
+                list($insertID, $rowsChanged) = \DB::insert('vicidial_list')->set($ppiLeadInsert)->execute('gabdialler');
+                    	    
+        	    $ppiAllLeads[] = array(
+                    'diallerid' => $insertID,
+        	        'forename' => $singleLead->forename,
+        	        'surname' => $singleLead->surname,
+        	        'address1' => $singleLead->street_and_number,
+        	        'address2' => $singleLead->area,
+        	        'address3' => $singleLead->district,
+        	        'town' => $singleLead->town,
+        	        'county' => $singleLead->county,
+        	        'postcode' => $singleLead->post_code,
+        	        'home' => $singleLead->tel_home,
+        	        'mobile' => $singleLead->tel_mobile,
+        	        'email' => $singleLead->email,
+        	        'dob' => date('d-m-Y', strtotime($singleLead->date_of_birth)),
+        	    );
+        	    
+        	    $lead = new \Model_Survey_Lead_Dialler();
+        	    $lead->referral_id = (int)$lead;
+        	    $lead->dialler_id = (int)$insertID;
+        	    $lead->type = "PPI";
+        	    $lead->save();
     	    
-    	    // Add leads directly to the dialler
-    	    
-            list($insertID, $rowsChanged) = \DB::insert('vicidial_list')->set($ppiLeadInsert)->execute('gabdialler');
-                	    
-    	    $ppiAllLeads[] = array(
-                'diallerid' => $insertID,
-    	        'forename' => $singleLead->forename,
-    	        'surname' => $singleLead->surname,
-    	        'address1' => $singleLead->street_and_number,
-    	        'address2' => $singleLead->area,
-    	        'address3' => $singleLead->district,
-    	        'town' => $singleLead->town,
-    	        'county' => $singleLead->county,
-    	        'postcode' => $singleLead->post_code,
-    	        'home' => $singleLead->tel_home,
-    	        'mobile' => $singleLead->tel_mobile,
-    	        'email' => $singleLead->email,
-    	        'dob' => date('d-m-Y', strtotime($singleLead->date_of_birth)),
-    	    );
-    	    
-    	    $lead = new \Model_Survey_Lead_Dialler();
-    	    $lead->referral_id = (int)$lead;
-    	    $lead->dialler_id = (int)$insertID;
-    	    $lead->type = "PPI";
-    	    $lead->save();
+    	    }
     	    
 	    }
 	    
 	    $drAllLeads = Array();
 	    foreach ($drLeads as $lead)
 	    {
-    	    $singleLead = \Model_Crmreferral::find($lead);
 
-    	    $drLeadInsert = array(
-                'lead_id'                 => "",
-                'entry_date'              => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
-                'modify_date'             => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
-                'status'                  => "NEW",
-                'user'                    => "",
-                'vendor_lead_code'        => "",
-                'source_id'               => "",
-                'list_id'                 => 149999,
-                'gmt_offset_now'          => 0.00,
-                'called_since_last_reset' => "N",
-                'phone_code'              => "9",
-                'phone_number'            => (int)(is_null($singleLead->tel_home)) ? $singleLead->tel_mobile : $singleLead->tel_home,
-                'title'                   => "",
-                'first_name'              => $singleLead->forename,
-                'middle_initial'          => "",
-                'last_name'               => $singleLead->surname,
-                'address1'                => $singleLead->street_and_number,
-                'address2'                => $singleLead->area,
-                'address3'                => $singleLead->district,
-                'city'                    => $singleLead->town,
-                'state'                   => "",
-                'province'                => $singleLead->county,
-                'postal_code'             => $singleLead->post_code,
-                'country_code'            => "UK",
-                'gender'                  => "U",
-                'date_of_birth'           => date('Y-m-d', strtotime($singleLead->date_of_birth)),
-                'alt_phone'               => ((int)$singleLead->tel_mobile == 0) ? "" : (int)$singleLead->tel_mobile,
-                'email'                   => "",
-                'security_phrase'         => "",
-                'comments'                => "!!! SURVEY LEAD !!! - DR QUALIFIED",
-                'called_count'            => 0,
-                'last_local_call_time'    => "2009-01-01 00:00:00",
-                'rank'                    => 0,
-                'owner'                   => "",
-                'entry_list_id'           => 0,
-            );
+    	    $thisCheck = Model_Survey_Lead_Dialler::query()->where('referral_id', $lead);
     	    
-    	    // Add leads directly to the dialler
-    	    
-            list($insertID, $rowsChanged) = \DB::insert('vicidial_list')->set($drLeadInsert)->execute('gabdialler');
-            
-            $drAllLeads[] = array(
-                'diallerid' => $insertID,
-    	        'forename' => $singleLead->forename,
-    	        'surname' => $singleLead->surname,
-    	        'address1' => $singleLead->street_and_number,
-    	        'address2' => $singleLead->area,
-    	        'address3' => $singleLead->district,
-    	        'town' => $singleLead->town,
-    	        'county' => $singleLead->county,
-    	        'postcode' => $singleLead->post_code,
-    	        'home' => $singleLead->tel_home,
-    	        'mobile' => $singleLead->tel_mobile,
-    	        'email' => $singleLead->email,
-    	        'dob' => date('d-m-Y', strtotime($singleLead->date_of_birth)),
-    	    );
-    	    
-    	    $lead = new \Model_Survey_Lead_Dialler();
-    	    $lead->referral_id = (int)$lead;
-    	    $lead->dialler_id = (int)$insertID;
-    	    $lead->type = "DR";
-    	    $lead->save();
+    	    if ($thisCheck->count() < 1)
+    	    {
+        	    
+        	    $singleLead = \Model_Crmreferral::find($lead);
+    
+        	    $drLeadInsert = array(
+                    'lead_id'                 => "",
+                    'entry_date'              => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
+                    'modify_date'             => date("Y-m-d H:i:s",strtotime($singleLead->referral_date)),
+                    'status'                  => "NEW",
+                    'user'                    => "",
+                    'vendor_lead_code'        => "",
+                    'source_id'               => "",
+                    'list_id'                 => 149999,
+                    'gmt_offset_now'          => 0.00,
+                    'called_since_last_reset' => "N",
+                    'phone_code'              => "9",
+                    'phone_number'            => (int)(is_null($singleLead->tel_home)) ? $singleLead->tel_mobile : $singleLead->tel_home,
+                    'title'                   => "",
+                    'first_name'              => $singleLead->forename,
+                    'middle_initial'          => "",
+                    'last_name'               => $singleLead->surname,
+                    'address1'                => $singleLead->street_and_number,
+                    'address2'                => $singleLead->area,
+                    'address3'                => $singleLead->district,
+                    'city'                    => $singleLead->town,
+                    'state'                   => "",
+                    'province'                => $singleLead->county,
+                    'postal_code'             => $singleLead->post_code,
+                    'country_code'            => "UK",
+                    'gender'                  => "U",
+                    'date_of_birth'           => date('Y-m-d', strtotime($singleLead->date_of_birth)),
+                    'alt_phone'               => ((int)$singleLead->tel_mobile == 0) ? "" : (int)$singleLead->tel_mobile,
+                    'email'                   => "",
+                    'security_phrase'         => "",
+                    'comments'                => "!!! SURVEY LEAD !!! - DR QUALIFIED",
+                    'called_count'            => 0,
+                    'last_local_call_time'    => "2009-01-01 00:00:00",
+                    'rank'                    => 0,
+                    'owner'                   => "",
+                    'entry_list_id'           => 0,
+                );
+        	    
+        	    // Add leads directly to the dialler
+        	    
+                list($insertID, $rowsChanged) = \DB::insert('vicidial_list')->set($drLeadInsert)->execute('gabdialler');
+                
+                $drAllLeads[] = array(
+                    'diallerid' => $insertID,
+        	        'forename' => $singleLead->forename,
+        	        'surname' => $singleLead->surname,
+        	        'address1' => $singleLead->street_and_number,
+        	        'address2' => $singleLead->area,
+        	        'address3' => $singleLead->district,
+        	        'town' => $singleLead->town,
+        	        'county' => $singleLead->county,
+        	        'postcode' => $singleLead->post_code,
+        	        'home' => $singleLead->tel_home,
+        	        'mobile' => $singleLead->tel_mobile,
+        	        'email' => $singleLead->email,
+        	        'dob' => date('d-m-Y', strtotime($singleLead->date_of_birth)),
+        	    );
+        	    
+        	    $lead = new \Model_Survey_Lead_Dialler();
+        	    $lead->referral_id = (int)$lead;
+        	    $lead->dialler_id = (int)$insertID;
+        	    $lead->type = "DR";
+        	    $lead->save();
+        	    
+    	    }
     	    
 	    }
 
