c31b105614eb75927fdd6963ee4f873b0868e4fc
Simon Skinner <simonskinner@dk101.net>
2013-05-23 12:43:34 +0100

    ADAM moving system

--- fuel/app/views/emails/dialler/ranking.php
+++ fuel/app/views/emails/dialler/ranking.php
@@ -50,6 +50,7 @@
 					<TABLE width="550" cellpadding="2" cellspacing="0">
 						<THEAD>
 							<TR style="background-color: #FFFFFF; border-bottom: #CCCCCC 1px solid; border-top: #CCCCCC 1px solid;">
+								<TH width="50">Center</TH>
 								<TH>Name</TH>
 								<TH width="100">Referrals</TH>
 								<TH width="100">Pack Outs</TH>
@@ -60,6 +61,7 @@
 						<TBODY>
 						    <?php foreach($top as $email_data): ?>
 							<TR style="background-color: #EEEEEE; border-bottom: #CCCCCC 1px solid;">
+							    <TD align="center"><?php echo $email_data['center']; ?></TD>
 								<TD align="center"><?php echo $email_data['name']; ?></TD>
 								<TD align="center"><?php echo $email_data['referrals']; ?></TD>
 								<TD align="center"><?php echo $email_data['packouts']; ?></TD>
@@ -78,6 +80,7 @@
 					<TABLE width="550" cellpadding="2" cellspacing="0">
 						<THEAD>
 							<TR style="background-color: #FFFFFF; border-bottom: #CCCCCC 1px solid; border-top: #CCCCCC 1px solid;">
+							    <TH width="50">Center</TH>
 								<TH>Name</TH>
 								<TH width="100">Referrals</TH>
 								<TH width="100">Pack Outs</TH>
@@ -88,6 +91,7 @@
 						<TBODY>
 						    <?php foreach($bottom as $email_data): ?>
 							<TR style="background-color: #EEEEEE; border-bottom: #CCCCCC 1px solid;">
+							    <TD align="center"><?php echo $email_data['center']; ?></TD>
 								<TD align="center"><?php echo $email_data['name']; ?></TD>
 								<TD align="center"><?php echo $email_data['referrals']; ?></TD>
 								<TD align="center"><?php echo $email_data['packouts']; ?></TD>
