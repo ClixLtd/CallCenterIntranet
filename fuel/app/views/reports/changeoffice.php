87a69fdb6d8af97172254612586bd8d811ee7845
Simon Skinner <vultuk@gmail.com>
2013-06-17 23:57:54 +0100

    Change Office System

---
+++ fuel/app/views/reports/changeoffice.php
@@ -0,0 +1,16 @@
+<article class="full-block clearfix">
+
+	<div class="article-container">
+		<header>
+			<h2>Change Office</h2>
+		</header>
+	</div>
+	
+	<section>
+		<ul>
+			<li>Leadpool ID: </li>
+			<li>Office: </li>
+			<li>Telesales: </li>
+		</ul>
+	</section>
+</article>
