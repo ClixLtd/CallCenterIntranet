7c423fa86c615147417a69a55a87d15302e604f0
Simon Skinner <vultuk@gmail.com>
2013-06-18 12:34:42 +0100

    Adding telesales for externals

--- fuel/app/classes/controller/reports.php
+++ fuel/app/classes/controller/reports.php
@@ -2436,7 +2436,10 @@ GROUP BY
 			       ELSE
 			       	DI_REF.short_code
 			       END AS Office
-			      ,(
+			      ,CASE WHEN
+			      	 DI_REF.short_code = 'REACTIV'
+			       THEN
+				     (
 				       SELECT Top (1)
 				         Undersigned
 				       FROM
@@ -2445,7 +2448,9 @@ GROUP BY
 				         Debtsolv.dbo.Client_LeadData AS D_CLD ON D_URS.ID = D_CLD.TelesalesAgent
 				       WHERE
 				         D_CLD.LeadPoolReference = CLD.ClientID
-				     )  AS 'Telesales Agent',
+				     )
+			       ELSE
+			         ISNULL(DI_REF.full_name, 'NONE') END AS 'Telesales Agent',
 			      
 			      ISNULL((
 			        SELECT Top (1)
@@ -2547,16 +2552,7 @@ GROUP BY
 			       ELSE
 			       	DI_REF.short_code
 			       END AS Office
-			      , (
-				       SELECT Top (1)
-				         Undersigned
-				       FROM
-				         Debtsolv.dbo.Users AS D_URS
-				       LEFT JOIN
-				         Debtsolv.dbo.Client_LeadData AS D_CLD ON D_URS.ID = D_CLD.TelesalesAgent
-				       WHERE
-				         D_CLD.LeadPoolReference = CLD.ClientID
-				     ) AS 'Telesales Agent',
+			      , ISNULL(DI_REF.full_name, 'NONE') AS 'Telesales Agent',
 			      
 			      ISNULL((
 			        SELECT Top (1)
