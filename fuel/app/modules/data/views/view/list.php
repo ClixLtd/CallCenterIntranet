020f234d0cde828eb952a723f0040b1c5738459c
Simon Skinner <vultuk@gmail.com>
2013-06-25 15:26:36 +0100

    Update basic count stats

---
+++ fuel/app/modules/data/views/view/list.php
@@ -0,0 +1,32 @@
+<div class="container">
+       
+	<table class="datatable">
+		<thead>
+			<tr>
+				<th>List ID</th>
+				<th>Purchased</th>
+				<th>Dialable</th>
+				<th>Contacted</th>
+				<th>Referrals</th>
+				<th>Pack Out</th>
+				<th>Pack In</th>
+				<th>Paid</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($lists as $list): ?>
+			<tr>
+				<td><?php echo $list['dialler_id']; ?></td>
+				<td><?php echo $list['purchased_leads']; ?></td>
+				<td><?php echo $list['dialable_leads']; ?></td>
+				<td><?php echo $list['contacted_leads']; ?></td>
+				<td><?php echo $list['referrals']; ?></td>
+				<td><?php echo $list['pack_out']; ?></td>
+				<td><?php echo $list['pack_in']; ?></td>
+				<td><?php echo $list['first_payment']; ?></td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+</div>
