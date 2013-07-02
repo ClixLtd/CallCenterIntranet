cadfadad7e8d09c3ae64f9ac8b3eb69a771d06be
Simon Skinner <vultuk@gmail.com>
2013-07-02 10:08:10 +0100

    Fixing Wallboard issues

--- fuel/app/classes/GAB/debtsolv.php
+++ fuel/app/classes/GAB/debtsolv.php
@@ -763,7 +763,7 @@ class Debtsolv {
 			    	$return_array['pack_outs_value'] = $return_array['pack_outs_value'] + $result['DI'];
 			    }
 			    
-			    if (isset($result['MyProduct']) AND (int)$result['MyProduct'] <> 2)
+			    if (isset($result['MyProduct']))
 			    {
     			    $return_array['referrals']++;
 			    }
