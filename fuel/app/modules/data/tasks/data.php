d79d05e610e748bf1086386fe7bde924d02df4c9
Simon Skinner <vultuk@gmail.com>
2013-07-17 16:36:42 +0100

    Local Cache of Dialler Statuses

--- fuel/app/modules/data/tasks/data.php
+++ fuel/app/modules/data/tasks/data.php
@@ -96,7 +96,7 @@ class Data
 	    
 	    foreach ($allToCheck as $singleLead)
 	    {
-		    $diallerResult = \DB::select('status')->from('vicidial_list')->where('lead_id', $singleLead['dialler_lead_id'])->execute()->as_array();
+		    $diallerResult = \DB::select('status')->from('vicidial_list')->where('lead_id', $singleLead['dialler_lead_id'])->execute('dialler')->as_array();
 		    
 		    $result = \DB::update('data_dialler_copy')->set(array('current_status' => $diallerResult[0]['status']))->where('dialler_lead_id', $singleLead['dialler_lead_id'])->execute();
 	    }
