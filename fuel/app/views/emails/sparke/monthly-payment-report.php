51b3e3f3cc00b7aa1372b7b1c05fcdfa2b262022
David Stansfield <dave@clixmedia.co.uk>
2013-09-12 19:56:28 +0100

    Layout change

--- fuel/app/views/emails/sparke/monthly-payment-report.php
+++ fuel/app/views/emails/sparke/monthly-payment-report.php
@@ -3,8 +3,10 @@
   <head></head>
   <body>
     <h2>Spark E Monthly Payment Report for <?=$month;?></h2>
-    
-    <table cellpadding="5" cellspacing="0">
+    <p>
+    The payment to make column is the double the DI minus the &pound;100 paid for the for the pack-in.
+    </p><p>
+    <table cellpadding="5" cellspacing="0" width="100%">
       <thead>
         <tr style="background-color:#DDDDDD">
           <th>Client ID</th>
@@ -24,12 +26,12 @@
         {
           ?>
           <tr style="background-color: <?php echo ($background2 == 0) ? "#EEEEEE" : "#CCCCCC"; ?>; border-bottom: #DDDDDD 1px solid;">
-            <td><?=$client['ClientID'];?></td>
-            <td><?=$client['FirstPaymentDate'];?></td>
-            <td><?=$client['SecondPaymentDate'];?></td>
-            <td><?=$client['UsedDI'];?></td>
-            <td><?=$client['PaymentMethod'];?></td>
-            <td>&pound;<?=$client['PaymentToMake'];?></td>
+            <td align="right"><?=$client['ClientID'];?></td>
+            <td align="right"><?=date("d-m-Y", strtotime($client['FirstPaymentDate']));?></td>
+            <td align="right"><?=date("d-m-Y", strtotime($client['SecondPaymentDate']));?></td>
+            <td align="right">&pound;<?=$client['UsedDI'];?></td>
+            <td align="center"><?=$client['PaymentMethod'];?></td>
+            <td align="right">&pound;<?=$client['PaymentToMake'];?></td>
           </tr>
           <?php
           $background2 = ($background2 == 0) ? 1 : 0;
@@ -46,5 +48,6 @@
       ?>
       </tbody>
     </table>
+    </p>
   </body>
 </html>
