bf401e16735f490d4b2fbb7d5f759a004f63cf09
Simon Skinner <codewith@vultuk.me>
2013-10-23 18:26:10 +0100

    Update data.php

--- fuel/app/modules/data/classes/controller/data.php
+++ fuel/app/modules/data/classes/controller/data.php
@@ -111,7 +111,7 @@ class Controller_Data extends \Controller_BaseHybrid
 	
 	$data = \Format::forge($makeArray)->to_csv();
 	    
-	$response = new Response();
+	$response = new \Response();
 	$response->set_header('Content-Type', 'application/pdf');
 	$response->set_header('Content-Disposition', 'attachment; filename="'.$data_id.'_'.date('Ymd-Hi').'.csv"');
 	
