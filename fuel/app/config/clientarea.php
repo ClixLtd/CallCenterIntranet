13a911fe25afcf5f455a918b8d773d59a95afee4
David Stansfield <dave@clixmedia.co.uk>
2014-02-17 12:40:16 +0000

    Changed Debtsolv databases

--- fuel/app/config/clientarea.php
+++ fuel/app/config/clientarea.php
@@ -4,7 +4,7 @@ return array(
 
   // -- Expert Money Solutions Connection
   // ------------------------------------
-  'expertmoneysolutions' => array(
+  'clixmedia' => array(
     'type'       => 'pdo',
     'connection' => array(
       'dsn'       => 'dblib:host=192.168.3.31:1433;dbname=LeadPool_MMS',
@@ -16,22 +16,6 @@ return array(
     'Identifier'  => '' ,
     'Charset'     => '',
     'profiling'   => true,
-  ),
-
-  // -- Expert Money Solutions Connection
-  // ------------------------------------
-  'clixmedia' => array(
-      'type'       => 'pdo',
-      'connection' => array(
-          'dsn'       => 'dblib:host=192.168.3.31:1433;dbname=LeadPool_MMS',
-          #'dsn'        => 'sqlsrv:Server=192.168.3.31,1433;Database=LeadPool_MMS',
-          'username'	 => 'debtsolv',
-          'password'	 => '76GerZnu871',
-          'persistent' => false,
-      ),
-      'Identifier'  => '' ,
-      'Charset'     => '',
-      'profiling'   => true,
   ),
 		
 );
